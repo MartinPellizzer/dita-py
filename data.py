ec2c08d109dabc6272d7731681519bade16e4a61
Martin Pellizzer <martinpellizzer@gmail.com>
2025-09-17 22:25:01 +0200

    '

--- data.py
+++ data.py
@@ -1,15 +1,21 @@
-DISPLAY_PAGE_HOME_POWER_ON = f'''Se vuoi avviare il generatore, clicca l'icona "POWER" (switch verde, posizione destra).'''
-DISPLAY_PAGE_HOME_POWER_OFF = f'''Clicca l'icona "POWER" per spegnere il generatore (switch rosso, posizione sinistra).'''
+DISPLAY_PAGE_HOME_POWER_ON = f'''Se vuoi avviare il generatore, clicca l'icona "SWITCH" (switch verde, posizione destra).'''
+DISPLAY_PAGE_HOME_POWER_OFF = f'''Clicca l'icona "SWITCH" per spegnere il generatore (switch rosso, posizione sinistra).'''
 DISPLAY_PAGE_HOME_SETTINGS = f'''Clicca l'icona "IMPOSTAZIONI".'''
 DISPLAY_PAGE_HOME_CALENDAR_ENABLE = f'''Verifica che l'opzione "ABILITA CALENDARIO" sia impostata su "ON".'''
 DISPLAY_PAGE_HOME_CALENDAR_DISABLE = f'''Verifica che l'opzione "ABILITA CALENDARIO" sia impostata su "OFF".'''
+DISPLAY_PAGE_HOME_GENERATORS = f'''Verifica che il valore di "NUMERO GENERATORI USATI" corrisponda a quello appena impostato.'''
 
 DISPLAY_PAGE_PASSWORD = f'''Inserisci la password e clicca il pulsante "OK" per continuare.'''
 
 DISPLAY_PAGE_SETTINGS_CALENDAR_ENABLE = f'''Clicca l'icona "ABILITA CALENDARIO".'''
+DISPLAY_PAGE_SETTINGS_GENERATORS = f'''Clicca l'icona "POWER".'''
+
+DISPLAY_PAGE_GENERATORS_ARROWS = f'''Clicca l'icona "FRECCIA IN ALTO" o "FRECCIA IN BASSO" per selezionare il numero di generatori attivi (minimo 1, massimo 3). Maggiore e il numero di generatori attivi, maggiore e la produzione di ozono.'''
 
 DISPLAY_PAGE_CALENDAR_ENABLE_ON = f'''Clicca l'icona "FRECCIA IN ALTO" per selezionare l'opzione "ON".'''
 DISPLAY_PAGE_CALENDAR_ENABLE_OFF = f'''Clicca l'icona "FRECCIA IN BASSO" per selezionare l'opzione "OFF".'''
 DISPLAY_PAGE_CALENDAR_ENABLE_SAVE = f'''Clicca l'icona "SALVA" per confermare la selezione.'''
 
 DISPLAY_PAGE_SETTINGS_BACK = f'''Clicca l'icona "INDIETRO" per tornare alla pagina principale.'''
+
+DISPLAY_ICON_SAVE = f'''Clicca l'icona "SALVA" per confermare la selezione.'''
--- task_gen.py
+++ task_gen.py
@@ -2,16 +2,69 @@ import g
 import data
 
 def gen():
+    power_set = f'''
+        <?cml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE topic PUBLIC "-//OASIS//DTD DITA Topic//EN" "topic.dtd">
+        <task id="power_set">
+            <title>Come Impostare la Produzione di Ozono</title>
+            <taskbody>
+                <context>
+                    <image href="projects/aurora/resources/images/demo_p_home_off.png" alt=""/>
+                    <image href="projects/aurora/resources/images/demo_generators_p_home_set.png" alt=""/>
+                    <image href="projects/aurora/resources/images/demo_p_password.png" alt=""/>
+                    <image href="projects/aurora/resources/images/demo_generators_p_set_power.png" alt=""/>
+                    <image href="projects/aurora/resources/images/demo_p_generators_select.png" alt=""/>
+                    <image href="projects/aurora/resources/images/demo_p_generators_save.png" alt=""/>
+                    <image href="projects/aurora/resources/images/demo_p_set_back.png" alt=""/>
+                    <image href="projects/aurora/resources/images/demo_p_home_generators.png" alt=""/>
+                    <image href="projects/aurora/resources/images/demo_p_home_power_on.png" alt=""/>
+                </context>
+                <steps>
+                    <step>
+                        <cmd>{data.DISPLAY_PAGE_HOME_POWER_OFF}</cmd>
+                    </step>
+                    <step>
+                        <cmd>{data.DISPLAY_PAGE_HOME_SETTINGS}</cmd>
+                    </step>
+                    <step>
+                        <cmd>{data.DISPLAY_PAGE_PASSWORD}</cmd>
+                    </step>
+                    <step>
+                        <cmd>{data.DISPLAY_PAGE_SETTINGS_GENERATORS}</cmd>
+                    </step>
+                    <step>
+                        <cmd>{data.DISPLAY_PAGE_GENERATORS_ARROWS}</cmd>
+                    </step>
+                    <step>
+                        <cmd>{data.DISPLAY_ICON_SAVE}</cmd>
+                    </step>
+                    <step>
+                        <cmd>{data.DISPLAY_PAGE_SETTINGS_BACK}</cmd>
+                    </step>
+                    <step>
+                        <cmd>{data.DISPLAY_PAGE_HOME_GENERATORS}</cmd>
+                    </step>
+                    <step>
+                        <cmd>{data.DISPLAY_PAGE_HOME_POWER_ON}</cmd>
+                    </step>
+                </steps>
+                <result>
+                    <p>The generator now produces ozone only during the times set in the calendar.</p>
+                </result>
+            </taskbody>
+        </task>
+    '''.strip()
+    with open(f'{g.TASKS_FOLDERPATH}/power_set.dita', 'w') as f: f.write(power_set)
+
     calendar_enable = f'''
         <?cml version="1.0" encoding="UTF-8"?>
         <!DOCTYPE topic PUBLIC "-//OASIS//DTD DITA Topic//EN" "topic.dtd">
-
         <task id="calendar-enable">
             <title>Come Abilitare il Calendario</title>
             <taskbody>
                 <context>
-                    <image href="projects/aurora/resources/images/demo_p_home_power_off.png" alt=""/>
-                    <image href="projects/aurora/resources/images/demo_p_home_set.png" alt=""/>
+                    <image href="projects/aurora/resources/images/demo_p_home_off.png" alt=""/>
+                    <image href="projects/aurora/resources/images/demo_generators_p_home_set.png" alt=""/>
                     <image href="projects/aurora/resources/images/demo_p_password.png" alt=""/>
                     <image href="projects/aurora/resources/images/demo_p_set_cal.png" alt=""/>
                     <image href="projects/aurora/resources/images/demo_p_cal_on.png" alt=""/>
@@ -60,13 +113,12 @@ def gen():
     calendar_disable = f'''
         <?cml version="1.0" encoding="UTF-8"?>
         <!DOCTYPE topic PUBLIC "-//OASIS//DTD DITA Topic//EN" "topic.dtd">
-
         <task id="calendar-disable">
             <title>Come Disabilitare il Calendario</title>
             <taskbody>
                 <context>
-                    <image href="projects/aurora/resources/images/demo_p_home_power_off.png" alt=""/>
-                    <image href="projects/aurora/resources/images/demo_p_home_set.png" alt=""/>
+                    <image href="projects/aurora/resources/images/demo_p_home_off.png" alt=""/>
+                    <image href="projects/aurora/resources/images/demo_generators_p_home_set.png" alt=""/>
                     <image href="projects/aurora/resources/images/demo_p_password.png" alt=""/>
                     <image href="projects/aurora/resources/images/demo_p_set_cal.png" alt=""/>
                     <image href="projects/aurora/resources/images/demo_p_cal_off.png" alt=""/>
@@ -89,7 +141,7 @@ def gen():
                         <cmd>{data.DISPLAY_PAGE_SETTINGS_CALENDAR_ENABLE}</cmd>
                     </step>
                     <step>
-                        <cmd>{data.DISPLAY_PAGE_CALENDAR_ENABLE_ON}</cmd>
+                        <cmd>{data.DISPLAY_PAGE_CALENDAR_ENABLE_OFF}</cmd>
                     </step>
                     <step>
                         <cmd>{data.DISPLAY_PAGE_CALENDAR_ENABLE_SAVE}</cmd>
